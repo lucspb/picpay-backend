f1ed9420a20a28ddb60b0ee33e69cb6476e6c666
Lucas Estanislau <lucasestanislau@hotmail.com>
2024-06-04 09:34:49 -0300

    feat: criacao do repository do User

---
+++ src/main/java/com/picpayproj/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.picpayproj.repositories;
+
+import com.picpayproj.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+}
