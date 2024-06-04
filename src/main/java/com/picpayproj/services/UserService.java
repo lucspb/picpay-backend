ab8943ff2813afcb6ff3763d4896b1467031c039
Lucas Estanislau <lucasestanislau@hotmail.com>
2024-06-04 14:58:30 -0300

    feat: criacao do service do User

---
+++ src/main/java/com/picpayproj/services/UserService.java
@@ -0,0 +1,34 @@
+package com.picpayproj.services;
+
+import com.picpayproj.domain.user.User;
+import com.picpayproj.domain.user.UserType;
+import com.picpayproj.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuário do tipo lojista não está autorizado a realizar transação");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente para saque");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.userRepository.findById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+}
