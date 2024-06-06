4c2cad627143f5c7e06b8dc3f810006c4ea1155e
Lucas Estanislau <lucasestanislau@hotmail.com>
2024-06-06 10:04:55 -0300

    fix: ajuste do notification service

--- src/main/java/com/picpayproj/services/NotificationService.java
+++ src/main/java/com/picpayproj/services/NotificationService.java
@@ -3,16 +3,11 @@ package com.picpayproj.services;
 import com.picpayproj.domain.user.User;
 import com.picpayproj.dtos.NotificationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class NotificationService {
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     public void sendNotification(User user, String message) throws Exception{
         String email = user.getEmail();
