2130f543782053267da0185d62d1afcbb6de288c
Lucas Estanislau <lucasestanislau@hotmail.com>
2024-06-06 10:06:14 -0300

    feat: criacao do controller das transactions

--- src/main/java/com/picpayproj/services/TransactionService.java
+++ src/main/java/com/picpayproj/services/TransactionService.java
@@ -26,7 +26,10 @@ public class TransactionService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void createTransaction(TransactionDTO transaction) throws Exception {
+    @Autowired
+    private NotificationService notificationService;
+
+    public Transaction createTransaction(TransactionDTO transaction) throws Exception {
         User sender = this.userService.findUserById(transaction.senderId());
         User receiver = this.userService.findUserById(transaction.receiverId());
 
@@ -49,6 +52,11 @@ public class TransactionService {
         this.transactionRepository.save(novaTransaction);
         this.userService.saveUser(sender);
         this.userService.saveUser(receiver);
+
+        this.notificationService.sendNotification(sender, "Transação efetuada com sucesso!");
+        this.notificationService.sendNotification(receiver, "Transação recebida com sucesso!");
+
+        return novaTransaction;
     }
 
     public boolean authorizeTransaction(User sender, BigDecimal value){
