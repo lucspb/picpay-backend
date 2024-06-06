a84657da77ec8af1ede360923b0e5d402d86ca15
Lucas Estanislau <lucasestanislau@hotmail.com>
2024-06-06 10:04:18 -0300

    fix: ajuste do notification service

--- src/main/java/com/picpayproj/services/NotificationService.java
+++ src/main/java/com/picpayproj/services/NotificationService.java
@@ -18,12 +18,14 @@ public class NotificationService {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
-        ResponseEntity<String> notificationResponse= restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+//        ResponseEntity<String> notificationResponse= restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+//
+//        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+//            System.out.println("Notification failed");
+//            throw new Exception("Serviço de notificação está indisponível");
+//
+//        }
 
-        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
-            System.out.println("Notification failed");
-            throw new Exception("Serviço de notificação está indisponível");
-
-        }
+        System.out.println("Notificação enviada!");
     }
 }
